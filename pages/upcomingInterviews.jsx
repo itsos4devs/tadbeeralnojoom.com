96a7887a27efea7103a909431e88374125cdfe2b
karimkhaled0 <kkmawe@gmail.com>
2023-02-19 01:45:01 +0200

    fix firebase

--- pages/upcomingInterviews.jsx
+++ pages/upcomingInterviews.jsx
@@ -16,7 +16,14 @@ const upcomingInterviews = () => {
   const { user, logout } = useUser();
   const { t } = useTranslation();
   const [snapshot] = useCollection(
-    query(collection(db, "users", user?.email, "upcomingInterviews"))
+    query(
+      collection(
+        db,
+        "users",
+        user?.email || "karimkhaledelmawe.gmail.com",
+        "upcomingInterviews"
+      )
+    )
   );
 
   return (
